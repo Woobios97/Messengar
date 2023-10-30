5e47e9a4f37aedcc7c60f0dc7f4b669adc647be2
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-10-30 21:20:24 +0900

    feature/로그인-등록UI 완성

---
+++ Messengar/Messengar/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,232 @@
+//
+//  RegisterViewController.swift
+//  Messengar
+//
+//  Created by 김우섭 on 10/30/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person")
+        imageView.tintColor = .gray
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let firstNameField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.attributedPlaceholder = NSAttributedString(
+            string: "성을 입력해주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]
+        )
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.textColor = .systemBackground
+        return field
+    }()
+    
+    private let lastNameField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.attributedPlaceholder = NSAttributedString(
+            string: "이름을 입력해주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]
+        )
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.textColor = .systemBackground
+        return field
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.attributedPlaceholder = NSAttributedString(
+            string: "이메일를 입력해주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]
+        )
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.textColor = .systemBackground
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.attributedPlaceholder = NSAttributedString(
+            string: "비밀번호를 입력해주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]
+        )
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        field.textColor = .systemBackground
+        return field
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("등록", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "로그인"
+        view.backgroundColor = .systemBackground
+        
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 50, height: 50)  // 여기에서 크기를 설정합니다
+        button.setTitle("등록", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)  // 버튼 텍스트 색상 설정
+        button.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
+        
+        let barButtonItem = UIBarButtonItem(customView: button)
+        navigationItem.rightBarButtonItem = barButtonItem
+        
+        registerButton.addTarget(self,
+                                 action: #selector(registerButtonTapped),
+                                 for: .touchUpInside)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        // Add Subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(firstNameField)
+        scrollView.addSubview(lastNameField)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(registerButton)
+        
+        imageView.isUserInteractionEnabled = true
+        //scrollView.isUserInteractionEnabled = true
+        
+        let gesture = UITapGestureRecognizer(target: self,
+                                             action: #selector(didTapChangeProfilePic))
+        imageView.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func didTapChangeProfilePic() {
+        print(#fileID, #function, #line, "this is - imageView터치")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (scrollView.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        firstNameField.frame = CGRect(x: 30,
+                                      y: imageView.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+        lastNameField.frame = CGRect(x: 30,
+                                     y: firstNameField.bottom + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+        emailField.frame = CGRect(x: 30,
+                                  y: lastNameField.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                     y: emailField.bottom + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+        registerButton.frame = CGRect(x: 30,
+                                      y: passwordField.bottom + 10,
+                                      width: scrollView.width - 60,
+                                      height: 52)
+    }
+    
+    @objc private func registerButtonTapped() {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        firstNameField.resignFirstResponder()
+        lastNameField.resignFirstResponder()
+        
+        guard let firstName = firstNameField.text,
+                let lastName = lastNameField.text,
+                let email = emailField.text,
+                let password = passwordField.text,
+                !email.isEmpty, !password.isEmpty, !firstName.isEmpty, !lastName.isEmpty,
+                password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+        
+        // Firebase Login
+    }
+    
+    func alertUserLoginError() {
+        let alert = UIAlertController(title: "확인",
+                                      message: "등록을 먼저해주세요",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "취소",
+                                      style: .cancel))
+        present(alert, animated: true)
+    }
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField {
+            registerButtonTapped()
+        }
+        return true
+    }
+}
