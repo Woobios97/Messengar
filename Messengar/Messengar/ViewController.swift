03dd17dbaf4e4c8159eef5ff02da23271d3e6408
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-10-30 16:58:46 +0900

    getting Started

---
+++ Messengar/Messengar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messengar
+//
+//  Created by 김우섭 on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
