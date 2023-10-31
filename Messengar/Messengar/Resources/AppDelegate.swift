7ba15eb7a62c5cb713358cbbe8e30e1ea1c20c66
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-11-01 00:41:55 +0900

    feature/로그인과 구글로그인구축

--- Messengar/Messengar/Resources/AppDelegate.swift
+++ Messengar/Messengar/Resources/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        
+        func application(_ app: UIApplication,
+                         open url: URL,
+                         options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+          return GIDSignIn.sharedInstance.handle(url)
+        }
+        
         
         return true
     }
