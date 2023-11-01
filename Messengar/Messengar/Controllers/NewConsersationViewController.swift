cdec8ce053d17c7e6e14155bef603d73ff4814d3
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-11-01 11:05:41 +0900

    feature/chat 구축

--- Messengar/Messengar/Controllers/NewConsersationViewController.swift
+++ Messengar/Messengar/Controllers/NewConsersationViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class NewConsersationViewController: UIViewController {
-
+    
+    private let spinner = JGProgressHUD()
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "유저를 검색해보세요"
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noResultsLabel: UILabel = {
+        let lable = UILabel()
+        lable.isHidden = true
+        lable.text = "결과가 없습니다,"
+        lable.textAlignment = .center
+        lable.textColor = .green
+        lable.font = .systemFont(ofSize: 21, weight: .medium)
+        return lable
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancle",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(dismissSelf))
+        searchBar.becomeFirstResponder()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+    
+}
 
+extension NewConsersationViewController: UISearchBarDelegate {
+    
 }
