5f26421d763553a27893364210db206abd55bbf9
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-11-01 19:30:33 +0900

    feature/유저딕셔너리name과email정보추가하기-유저검색기능구축

--- Messengar/Messengar/Controllers/ProfileViewController.swift
+++ Messengar/Messengar/Controllers/ProfileViewController.swift
@@ -20,8 +20,58 @@ class ProfileViewController: UIViewController {
                            forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.tableHeaderView = createTableHeader()
     }
     
+    func createTableHeader() -> UIView? {
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+        let fileName = safeEmail + "_profile_picture.png"
+        let path = "images/" + fileName
+        
+        let headerView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: self.view.width,
+                                              height: 300))
+        headerView.backgroundColor = .link
+        let imageView = UIImageView(frame: CGRect(x: (view.width - 150) / 2,
+                                                  y: 75,
+                                                  width: 150,
+                                                  height: 150))
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 3
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageView.width / 2
+        headerView.addSubview(imageView)
+        
+        StorageManager.shared.downloadURL(for: path, completion: { [weak self] result in
+            switch result {
+            case .success(let url):
+                self?.downloadImage(imageView: imageView, url: url)
+            case .failure(let error):
+                print(#fileID, #function, #line, "this is - URL다운로드 실패 \(error)")
+            }
+        })
+        return headerView
+    }
+    
+    func downloadImage(imageView: UIImageView, url: URL) {
+        URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                imageView.image = image
+            }
+        })
+        .resume()
+    }
     
 }
 
@@ -53,7 +103,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
                 try FirebaseAuth.Auth.auth().signOut()
                 let vc = LoginViewController()
                 let nav = UINavigationController(rootViewController: vc)
-                nav.modalPresentationStyle = .fullScreen 
+                nav.modalPresentationStyle = .fullScreen
                 // 로그인 후 화면닫기를 원하기 때문에 -> fullScreen
                 strongSelf.present(nav, animated: true)
             } catch {
